eb9bcbcb4ea523800e6f51c4d4481b1e10831183
Zezhi Shao <864453277@qq.com>
2024-03-15 23:06:55 +0800

    init

---
+++ main.py
@@ -0,0 +1,28 @@
+import time
+from utils import get_daily_papers_by_keyword, generate_table
+
+
+keywords = ["Time Series"] # TODO add more keywords
+
+max_result = 100 # NOTE this project only retains the latest 100 papers
+
+# all columns: Title, Authors, Abstract, Link, Tags, Comment, Date
+# fixed_columns = ["Title", "Link", "Date"]
+
+column_names = ["Title", "Link", "Abstract", "Date", "Comment"]
+
+top_desc = "This project only retains the latest 100 papers."
+
+with open("README.md", "w") as f:
+    f.write("# Daily Papers\n")
+    f.write("{0}\n".format(top_desc))
+    for keyword in keywords:
+        f.write("## {0}\n".format(keyword))
+        papers = get_daily_papers_by_keyword(keyword, column_names, max_result)
+        if len(papers) == 0:
+            print("ArXiv API Limit Exceeded!\n")
+            break
+        table = generate_table(papers)
+        f.write(table)
+        f.write("\n\n")
+        time.sleep(30) # avoid being blocked by arXiv API
---
+++ utils.py
@@ -0,0 +1,104 @@
+import feedparser
+from typing import List, Dict
+import urllib, urllib.request
+from easydict import EasyDict
+
+
+def request_paper_with_arXiv_api(keyword: str, max_results: int) -> List[Dict[str, str]]:
+    # keyword = keyword.replace(" ", "+")
+    keyword = "\"" + keyword + "\""
+    url = "http://export.arxiv.org/api/query?search_query=ti:{0}+OR+abs:{0}&max_results={1}&sortBy=submittedDate".format(keyword, max_results)
+    url = urllib.parse.quote(url, safe="%/:=&?~#+!$,;'@()*[]")
+    response = urllib.request.urlopen(url).read().decode('utf-8')
+    feed = feedparser.parse(response)
+
+    # NOTE default columns: Title, Authors, Abstract, Link, Tags, Comment, Date
+    papers = []
+    for entry in feed.entries:
+        entry = EasyDict(entry)
+        paper = EasyDict()
+
+        # title
+        paper.Title = entry.title.replace("\n", " ")
+        # abstract
+        paper.Abstract = entry.summary.replace("\n", " ")
+        # authors
+        paper.Authors = [_["name"].replace("\n", " ") for _ in entry.authors]
+        # link
+        paper.Link = entry.link.replace("\n", " ")
+        # tags
+        paper.Tags = [_["term"].replace("\n", " ") for _ in entry.tags]
+        # comment
+        paper.Comment = entry.get("arxiv_comment", "").replace("\n", " ")
+        # date
+        paper.Date = entry.updated
+
+        papers.append(paper)
+    return papers
+
+def filter_tags(papers: List[Dict[str, str]], target_fileds: List[str]=["cs", "stat"]) -> List[Dict[str, str]]:
+    # filtering tags: only keep the papers in target_fileds
+    results = []
+    for paper in papers:
+        tags = paper.Tags
+        for tag in tags:
+            if tag.split(".")[0] in target_fileds:
+                results.append(paper)
+                break
+    return results
+
+def get_daily_papers_by_keyword(keyword: str, column_names: List[str], max_result) -> List[Dict[str, str]]:
+    # get papers
+    papers = request_paper_with_arXiv_api(keyword, max_result) # NOTE default columns: Title, Authors, Abstract, Link, Tags, Comment, Date
+    # NOTE filtering tags: only keep the papers in cs field
+    # TODO filtering more
+    papers = filter_tags(papers)
+    # select columns for display
+    papers = [{column_name: paper[column_name] for column_name in column_names} for paper in papers]
+    return papers
+
+def generate_table(papers: List[Dict[str, str]]) -> str:
+    formatted_papers = []
+    keys = papers[0].keys()
+    for paper in papers:
+        # process fixed columns
+        formatted_paper = EasyDict()
+        ## Title and Link
+        formatted_paper.Title = "**" + "[{0}]({1})".format(paper["Title"], paper["Link"]) + "**"
+        ## Process Date (format: 2021-08-01T00:00:00Z -> 2021-08-01)
+        formatted_paper.Date = paper["Date"].split("T")[0]
+        
+        # process other columns
+        for key in keys:
+            if key in ["Title", "Link", "Date"]:
+                continue
+            elif key == "Abstract":
+                # add show/hide button for abstract
+                formatted_paper[key] = "<details><summary>Show</summary><p>{0}</p></details>".format(paper[key])
+            elif key == "Authors":
+                # NOTE only use the first author
+                formatted_paper[key] = paper[key][0] + " et al."
+            elif key == "Tags":
+                tags = ", ".join(paper[key])
+                if len(tags) > 10:
+                    formatted_paper[key] = "<details><summary>{0}...</summary><p>{1}</p></details>".format(tags[:5], tags)
+                else:
+                    formatted_paper[key] = tags
+            elif key == "Comment":
+                if paper[key] == "":
+                    formatted_paper[key] = ""
+                elif len(paper[key]) > 20:
+                    formatted_paper[key] = "<details><summary>{0}...</summary><p>{1}</p></details>".format(paper[key][:5], paper[key])
+        formatted_papers.append(formatted_paper)
+
+    # generate header
+    columns = formatted_papers[0].keys()
+    # highlight headers
+    columns = ["**" + column + "**" for column in columns]
+    header = "| " + " | ".join(columns) + " |"
+    header = header + "\n" + "| " + " | ".join(["---"] * len(formatted_papers[0].keys())) + " |"
+    # generate the body
+    body = ""
+    for paper in formatted_papers:
+        body += "\n| " + " | ".join(paper.values()) + " |"
+    return header + body
