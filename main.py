9aa88770dd2d745cca30dbe994e6687aee19b42e
S22 <864453277@qq.com>
2024-06-20 09:46:25 +0800

    remove date checking

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ with open("README.md", "r") as f:
         line = f.readline()
         if "Last update:" in line: break
     last_update_date = line.split(": ")[1].strip()
-    if last_update_date == current_date:
-        sys.exit("Already updated today!")
+    # if last_update_date == current_date:
+        # sys.exit("Already updated today!")
 
 keywords = ["Time Series", "Trajectory", "Graph Neural Networks"] # TODO add more keywords
 
